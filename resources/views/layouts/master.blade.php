9e18841a17b579faa7ca43d84d41b99829866d9f
Abu Sayem Amir <sayem74@gmail.com>
2023-11-27 08:32:57 +0600

    fixed project link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
 
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    {{-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"> --}}
 
     <link rel="stylesheet" href="{{ asset('assets/external/aos/aos.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/external/bootstrap/css/bootstrap.min.css') }}">
--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -30,7 +30,7 @@
                 <td class="text-center">{{ ++$key }}</td>
                 <td class="text-center">{{ $project['title'] }}</td>
                 <td>{{ $project['description'] }}</td>
-                <td>{{ $project['link'] }}</td>
+                <td> <a href="{{ $project['link'] }}" target="_blank">{{ $project['link'] }}</a></td>
                 
             </tr>
                 
